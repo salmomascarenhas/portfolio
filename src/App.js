f691d3dec608f4bbbb70c42c6427bd9afe4d8703
Salmo <salmo.cruz@gmail.com>
2020-08-07 00:23:01 -0300

    ddeploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Routes from './routes';
+import Intro from './pages/Intro';
 
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <Routes />
+    <Intro />
   );
 }
 
